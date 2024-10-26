351e7cf3810c32e5aa89cc24536b426edee1ae91
ikabbash <i.kabbash21@gmail.com>
2024-10-26 09:07:56 +0300

    update: Azure app insights and Terraform outputs

--- serverless/azure/terraform/main.tf
+++ serverless/azure/terraform/main.tf
@@ -9,6 +9,12 @@ resource "azurerm_storage_account" "te_storage_account" {
   }
 }
 
+resource "azurerm_storage_container" "te_blob_container" {
+  name                  = "transcripts"
+  storage_account_name  = azurerm_storage_account.te_storage_account.name
+  container_access_type = "private"
+}
+
 resource "azurerm_service_plan" "te_service_plan" {
   name                = "topics-extractor-service-plan"
   resource_group_name = azurerm_resource_group.te_resource_group.name
@@ -20,17 +26,29 @@ resource "azurerm_service_plan" "te_service_plan" {
   }
 }
 
-resource "azurerm_linux_function_app" "te_function_app" {
-  name                = "funcapp${random_id.rand_id.hex}"
+resource "azurerm_application_insights" "te_app_insights" {
+  name                = "appinsights${random_id.rand_id.hex}"
   resource_group_name = azurerm_resource_group.te_resource_group.name
   location            = azurerm_resource_group.te_resource_group.location
+  application_type    = "web"
+  tags = {
+    environment = "topics-extractor"
+  }
+}
+
+resource "azurerm_linux_function_app" "te_function_app" {
+  name                       = "funcapp${random_id.rand_id.hex}"
+  resource_group_name        = azurerm_resource_group.te_resource_group.name
+  location                   = azurerm_resource_group.te_resource_group.location
   storage_account_name       = azurerm_storage_account.te_storage_account.name
   storage_account_access_key = azurerm_storage_account.te_storage_account.primary_access_key
   service_plan_id            = azurerm_service_plan.te_service_plan.id
   site_config {
     application_stack {
-        python_version = "3.12"
+      python_version = "3.11"
     }
+    application_insights_connection_string = azurerm_application_insights.te_app_insights.connection_string
+    application_insights_key               = azurerm_application_insights.te_app_insights.instrumentation_key
   }
   tags = {
     environment = "topics-extractor"
