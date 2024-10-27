885babbf2ed1e969341b5649321a3e4ac464e433
ikabbash <i.kabbash21@gmail.com>
2024-10-27 20:36:24 +0300

    update: Terraform final touches

--- serverless/terraform/main.tf
+++ serverless/terraform/main.tf
@@ -4,11 +4,17 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_storage_bucket" "cloud_function_bucket" {
-  name     = "${var.project_id}-cloud-function-bucket"
-  location = var.region
+resource "random_id" "rand_id" {
+  byte_length = 4
 }
 
+resource "google_storage_bucket" "cloud_function_bucket" {
+  name          = "cloud-function-bucket-${random_id.rand_id.hex}"
+  location      = var.region
+  force_destroy = true
+}
+
+# Zip the main.py and requirements.txt files first
 resource "google_storage_bucket_object" "cloud_function_code" {
   name   = "topics_extractor.zip"
   bucket = google_storage_bucket.cloud_function_bucket.name
@@ -34,7 +40,6 @@ resource "google_project_iam_member" "cloud_function_secret_manager_accessor" {
   member  = data.google_app_engine_default_service_account.sa_default_app_engine.member
 }
 
-# For some reason Cloud Functions can't take regional secrets
 resource "google_secret_manager_secret" "gemini_api_secret" {
   secret_id = "GOOGLE_API_KEY"
   replication {
@@ -43,14 +48,14 @@ resource "google_secret_manager_secret" "gemini_api_secret" {
 }
 
 resource "google_secret_manager_secret_version" "regional_secret_version_basic" {
-  secret = google_secret_manager_secret.gemini_api_secret.id
+  secret      = google_secret_manager_secret.gemini_api_secret.id
   secret_data = var.google_api_key
 }
 
 resource "google_cloudfunctions_function" "cloud_function" {
-  name                = "topics_extractor"
-  runtime             = "python312"
-  available_memory_mb = 128 # CHANGE THIS
+  name                  = "topics_extractor"
+  runtime               = "python312"
+  available_memory_mb   = 256
   source_archive_bucket = google_storage_bucket.cloud_function_bucket.name
   source_archive_object = google_storage_bucket_object.cloud_function_code.name
   event_trigger {
@@ -59,9 +64,9 @@ resource "google_cloudfunctions_function" "cloud_function" {
   }
 
   secret_environment_variables {
-      key     = "GOOGLE_API_KEY"
-      project_id = var.project_id
-      secret  = google_secret_manager_secret.gemini_api_secret.secret_id
-      version = "latest"
+    key        = "GOOGLE_API_KEY"
+    project_id = var.project_id
+    secret     = google_secret_manager_secret.gemini_api_secret.secret_id
+    version    = "latest"
   }
 }
